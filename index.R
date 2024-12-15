b3ae0f362184f0016281357c53182f071721a720
Aryan Sultan Khan <ariansultan94@gmail.com>
2024-12-15 14:23:14 +1100

    deleted the index

---
+++ index.R
@@ -0,0 +1,22 @@
+---
+  format: html
+editor: visual
+html:
+  includes:
+  in-header: styles.css
+---
+  
+  
+  <link rel="stylesheet" href="styles.css">
+    
+    <link rel="preload" href="posts.html" as="document">
+      
+      <link rel="preload" href="about_styles.css" as="style">
+        
+        myimage.jpeg
+      
+      Welcome to My Insights!
+        
+        Hi, I am Aryan Sultan, an avid explorer of economic trends and forecasting methodologies. Here, I share my insights through blogs about topics related to economics, statistics, the science of forecasting, and more. My goal is to share knowledge and spark discussions on topics I find fascinating.
+      
+      You can learn more about me here.
